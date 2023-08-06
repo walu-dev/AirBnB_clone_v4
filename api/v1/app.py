7d9d0b743164eafb3e725b13402347a77687871e
walu-dev <jonathanenoch11@gmail.com>
2023-08-06 16:03:57 +0000

    task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,14 +4,14 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from flasgger import Swagger
 from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r'/api/v1/*': {'origins': '*'}})
 
 
 @app.teardown_appcontext
